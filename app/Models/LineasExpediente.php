02aa5ae88199414cfbe2cac46483d30c0d7ddbbd
leocodingworld <okidoki0305@hotmail.com>
2022-09-07 22:27:33 +0200

    asd

--- app/Models/LineasExpediente.php
+++ app/Models/LineasExpediente.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\PseudoTypes\True_;
 
 class LineasExpediente extends Model
 {
     protected $table = "lineasExpedientes";
-	protected $primaryKey = ["expediente", "linea"];
-	public $incrementing = false;
+	protected $primaryKey = "linea";
+	public $incrementing = true;
 	public $timestamps = false;
 
 	public function expediente() {
