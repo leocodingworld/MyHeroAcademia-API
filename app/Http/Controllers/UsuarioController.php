c66f05a2ddf42f624de57546defeb0d80f7a1b9c
leocodingworld <okidoki0305@hotmail.com>
2022-09-12 16:27:32 +0200

    xcvxv

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -35,7 +35,9 @@ class UsuarioController extends Controller
 			$this -> createPersonal($usuario);
 		}
 
-		return $usuario != null ? true : false;
+		return response() -> json([
+			"mensaje" => "Usuario creado con éxito"
+		]);
 	}
 
 	private function createAlumno(Usuario $usuario) {
@@ -74,7 +76,7 @@ class UsuarioController extends Controller
 	}
 
 	public function getUsuarioData($usuario) { // OK
-		return Usuario::where("id", $usuario) -> first();
+		return Usuario::find($usuario);
 	}
 
 	public function getAlumnos() { // Añadir ciertos campos extras
@@ -105,7 +107,9 @@ class UsuarioController extends Controller
 		$activar -> activo = true;
 		$activar -> save();
 
-		return true;
+		return response() -> json([
+
+		], 200);
 	}
 
 	public function desactivarUsuario(Request $request) {
