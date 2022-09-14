bc5aa850761f553e23b5b4339c7090a114c2bea5
Carlos Paez <carlos.paezd@hotmail.com>
2022-09-14 13:54:10 +0200

    asd

--- database/seeders/LineaExpedienteSeeder.php
+++ database/seeders/LineaExpedienteSeeder.php
@@ -10,67 +10,67 @@ use Illuminate\Database\Seeder;
 
 class LineaExpedienteSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-		$periodos = [
-			"1ª Evaluación",
-			"2ª Evaluación",
-			"1ª Evaluación Final Ordinaria",
-			"2ª Evaluación Final Ordinaria",
-			"1ª Evaluación Final Extraordinaria",
-			"2ª Evaluación Final Extraordinaria"
-		];
+		/**
+		 * Run the database seeds.
+		 *
+		 * @return void
+		 */
+		public function run()
+		{
+			$periodos = [
+				"1ª Evaluación",
+				"2ª Evaluación",
+				"1ª Evaluación Final Ordinaria",
+				"2ª Evaluación Final Ordinaria",
+				"1ª Evaluación Final Extraordinaria",
+				"2ª Evaluación Final Extraordinaria"
+			];
 
-		$cursos1 = Curso::all() -> take(4);
-		$cursos2 = Curso::all() -> skip(4) -> take(4);
+			$cursos1 = Curso::all() -> take(4);
+			$cursos2 = Curso::all() -> skip(4) -> take(4);
 
-		$expedientes1 = Expediente::all() -> take(8);
-		$expedientes2 = Expediente::all() -> skip(8) -> take(8);
+			$expedientes1 = Expediente::all() -> take(8);
+			$expedientes2 = Expediente::all() -> skip(8) -> take(8);
 
-		foreach($expedientes1 as $e) {
-			$i = 1;
-			$linea = new LineaExpediente();
+			foreach($expedientes1 as $e) {
+				$i = 1;
+				$linea = new LineaExpediente();
 
-			foreach($cursos1 as $c) {
+				foreach($cursos1 as $c) {
 
-				foreach($c -> modulos as $m) {
-					$linea -> numExpediente = $e -> numero;
-					$linea -> numero = $i;
-					$linea -> idCurso = $m -> idCurso;
-					$linea -> modulo = $m -> id;
-					$linea -> fecha = date("Y-m-d");
-					$linea -> periodo = $periodos[rand(0,5)];
-					$linea -> calificacion = rand(1,10);
+					foreach($c -> modulos as $m) {
+						$linea -> numExpediente = $e -> numero;
+						$linea -> numero = $i;
+						$linea -> idCurso = $m -> idCurso;
+						$linea -> modulo = $m -> id;
+						$linea -> fecha = date("Y-m-d");
+						$linea -> periodo = $periodos[rand(0,5)];
+						$linea -> calificacion = rand(1,10);
 
-					$linea -> save();
-					$i++;
+						$linea -> save();
+						$i++;
+					}
 				}
 			}
-		}
 
-		foreach($expedientes2 as $e) {
-			$i = 1;
-			$linea = new LineaExpediente();
+			foreach($expedientes2 as $e) {
+				$i = 1;
+				$linea = new LineaExpediente();
 
-			foreach($cursos2 -> modulos as $m) {
-				foreach($c -> modulos as $m) {
-					$linea -> numExpediente = $e -> numero;
-					$linea -> numero = $i;
-					$linea -> idCurso = $m -> idCurso;
-					$linea -> modulo = $m -> id;
-					$linea -> fecha = date("Y-m-d");
-					$linea -> periodo = $periodos[rand(0,5)];
-					$linea -> calificacion = rand(1,10);
+				foreach($cursos2 -> modulos as $m) {
+					foreach($c -> modulos as $m) {
+						$linea -> numExpediente = $e -> numero;
+						$linea -> numero = $i;
+						$linea -> idCurso = $m -> idCurso;
+						$linea -> modulo = $m -> id;
+						$linea -> fecha = date("Y-m-d");
+						$linea -> periodo = $periodos[rand(0,5)];
+						$linea -> calificacion = rand(1,10);
 
-					$linea -> save();
-					$i++;
+						$linea -> save();
+						$i++;
+					}
 				}
 			}
-		}
-    }
+			}
 }
